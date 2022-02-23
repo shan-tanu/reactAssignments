42e6e385294306c6c13671399bed4379cb8f9016
Shantanu M G <shantanumysore@gmail.com>
2022-02-23 11:37:41 +0530

    code correction

--- 22_2/employee_filter/src/classComponents/EmployeeFilter.js
+++ 22_2/employee_filter/src/classComponents/EmployeeFilter.js
@@ -3,27 +3,24 @@ import EmployeeListComponent from '../EmployeeListComponent';
 
 import empList from "../../src/data/employeeList";
 
-var globalState = {
-    employees: empList
-}
+// var globalState = {
+//     employees: empList
+// }
 
 export default class EmployeeFilter extends Component {
     constructor() {
         super();
-        this.state = globalState;
+        this.globalState = empList;
+        this.state = {employees: empList};
     };
 
-    changedSearch(event) {
+    changedSearch = (event) => {
         var searchStr = event.target.value;
         if (searchStr === "") {
-            this.setState(globalState)
+            this.setState({employees: this.globalState});
         } else {
-            var updatedState = globalState.employees.filter(x => x.name.toLowerCase().includes(searchStr.toLowerCase()))
-            this.setState(
-                {
-                    employees: updatedState
-                }
-            )
+            var updatedState = this.globalState.filter(x => x.name.toLowerCase().includes(searchStr.toLowerCase()));
+            this.setState({employees: updatedState});
         }
     }
 
@@ -31,12 +28,11 @@ export default class EmployeeFilter extends Component {
     render() {
         return (
             <>
-                <h1 style={{margin: "20px 12px", textAlign: "center", fontFamily: "'Fira Sans', sans-serif"}}>Employee Filter Application</h1>
-                <br/>
+                <h1 style={{margin: "20px 12px", textAlign: "center", fontFamily: "'Fira Sans', sans-serif"}}>Employee Filter Application</h1><br/>
                 {/* <!-- Search form --> */}
                 <div style={{margin:"16px 14px", padding:"12px"}}>
                     <h4>Enter name/keyword to search for :</h4>
-                    <input className="form-control" type="text" placeholder="Name" width="50px" aria-label="Search" onChange={(event) => this.changedSearch(event)} style={{marginTop:"16px"}}/>
+                    <input className="form-control" type="text" placeholder="Name" width="50px" aria-label="Search" onChange={this.changedSearch} style={{marginTop:"16px"}}/>
                 </div>
                 <EmployeeListComponent listOfEmployees={this.state.employees} />
             </>
