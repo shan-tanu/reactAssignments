29bc842d077c2139ed0b3d03c13c2d0fb1774fc0
Shantanu M G <shantanumysore@gmail.com>
2022-02-23 14:06:35 +0530

    stock display activity

---
+++ 23_2/stockAPI/StockDetailsComponent.js
@@ -0,0 +1,39 @@
+export default function StockDetailsComponent(props) {
+
+    // pending,name,qty,price
+
+    var styleObject = {
+        containerStyle: {
+            width: "18rem",
+            margin: "20px",
+            display: "inline-block"
+        }
+    };
+
+    return (
+        <div className="card" style={styleObject.containerStyle}>
+            <div className="card-body">
+                <p>Stock Symbol : <b>{props.data.symbol}</b></p>
+                <h2 className="card-title">{props.data.SC_FULLNM}</h2>
+                <p>Market Cap : {props.data.MKTCAP}</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Lower Circuit</th>
+                            <th>Upper Circuit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{props.data.lower_circuit_limit}</td>
+                            <td>{props.data.upper_circuit_limit}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br/>
+                <p>Current Price: <h2>{props.data.pricecurrent}</h2></p>
+
+            </div>
+        </div>
+    )
+}
